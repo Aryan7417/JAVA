66e7329ca8fa2869b596b9f99b18b94ddc64b983
Aryan Yadav <caryan830@gmail.com>
2026-04-18 02:16:58 +0530

    add patten problem

---
+++ new java/pattens.java
@@ -0,0 +1,172 @@
+//--------solid patten----------
+
+// public class pattens {
+//     public static void main(String[] args) {
+//         int n=7;
+//         int m=8;
+//         //outer loop
+//         for (int i = 1; i <= n; i++) {
+//             //inner loop
+//             for (int j = 1; j <= m; j++) {
+//                 System.out.print("*");
+
+//             }
+            
+//             System.out.println();
+
+
+//         }
+
+//     }
+// }
+
+
+//----------------------hollow Rectangle-------------------------
+
+// public class pattens {
+
+//     public static void main(String[] args) {
+
+//         int n = 5;
+//         int m =6;
+        
+
+//         for(int i =1;i<=n;i++){
+//             for (int j = 1; j<=m ;j++){
+//                 if(i==1 || j==1 || i==n ||j==m){
+
+//                     System.out.print("*");
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+                
+//             }
+//             System.out.println();
+//         }
+        
+//     }
+// }
+
+
+
+//------------------------half pyramid------------------------------
+
+
+// public class pattens {
+//     public static void main(String[] args) {
+//         int n = 4;
+//         for(int i =1 ;i<=n;i++){
+//             for(int j = 1;j<=i;j++){
+//                 System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+// }
+
+// }
+
+
+//------------------------inverted half pyramid-----------------------------------
+
+
+// public class pattens {
+
+//     public static void main(String[] args) {
+//         int n=5;
+//         for(int i=n;i>=1;i--){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+//-----------------invertyed half pyramid(rotated by 18-deg)------------------------------
+
+
+// public class pattens {
+
+//     public static void main(String[] args) {
+//         int n= 5;
+//         for (int i=1;i<=n;i++){
+//             for(int j = 1;j<=n-i;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j =1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+// }
+// }
+
+
+//--------------- half pyramid with number -------------------------
+
+
+// public class pattens {
+
+//     public static void main(String[] args) {
+//         int n = 5;
+
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=1;j++){
+//                 System.out.print(j+" "+j);
+//             }
+//         }
+
+
+
+//     }
+// }
+
+
+//-----------------------------------inverted half pyramiod with number------------
+
+// public class pattens {
+
+//     public static void main(String[] args) {
+//         int n= 5;
+
+//         for(int i= 1;i<=n;i++){
+//             for(int j=1;j<=n-i+1;j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+//---------------------floyd's triangle------------------------------
+
+// public class pattens {
+
+//     public static void main(String[] args) {
+//         int n=5;
+//         int number =1;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(number+ " ");
+//                 number++;
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
